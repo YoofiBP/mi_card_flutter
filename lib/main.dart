958af146524cadc2b66ac3463553f73dd19f5397
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2020-12-26 13:24:15 +0000

    First Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 65.0,
+                    backgroundImage:
+                        AssetImage('images/41188788375_a9d10bd38c_k.jpg'),
+                  ),
+                  Text(
+                    'Yoofi Brown-Pobee',
+                    style: TextStyle(
+                        fontFamily: 'Yellowtail',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25.0),
+                  ),
+                  Text(
+                    'Software Engineer'.toUpperCase(),
+                    style: TextStyle(
+                        fontFamily: 'PTSansNarrow',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 15.0),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
